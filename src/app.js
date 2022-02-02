41d842a36176c703f1263b2915237781fca8c110
ramon oliveira <ramon30012000@gmail.com>
2022-02-01 23:50:34 -0300

    feat: basic sign-up route for front-end test

--- src/app.js
+++ src/app.js
@@ -5,4 +5,9 @@ const app = express();
 app.use(json());
 app.use(cors());
 
+app.post("/sign-up", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+})
+
 app.listen(5000, console.log("Listening on 5000"));
