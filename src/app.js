d378eeaec73130d16f4c11e406110c337468695c
ramon oliveira <ramon30012000@gmail.com>
2022-02-02 16:54:45 -0300

    feat: basic sign-in route structure for test

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ app.use(cors());
 app.post("/sign-up", (req, res) => {
   console.log(req.body);
   res.sendStatus(200);
-})
+});
+
+app.post("/sign-in", (req, res) => {
+  console.log(req.body);
+
+  const authorizedUser = { ...req.body, token: "shsdf3sd2f12d1f3sd2" };
+  res.status(200).send(authorizedUser);
+});
 
 app.listen(5000, console.log("Listening on 5000"));
