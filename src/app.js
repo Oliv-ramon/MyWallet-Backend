139ef4fc10c87ef78ca586582dae22af917f1713
ramon oliveira <ramon30012000@gmail.com>
2022-02-03 17:01:26 -0300

    feat: all the validations in the sign-in and sign-up routes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,13 @@
 import express, { json } from "express";
 import cors from "cors";
+import { signIn, signUp } from "./controllers/authController.js";
 
 const app = express();
 app.use(json());
 app.use(cors());
 
-app.post("/sign-up", (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
-});
+app.post("/auth/sign-up", signUp);
 
-app.post("/sign-in", (req, res) => {
-  console.log(req.body);
-
-  const authorizedUser = { ...req.body, token: "shsdf3sd2f12d1f3sd2" };
-  res.status(200).send(authorizedUser);
-});
+app.post("/auth/sign-in", signIn);
 
 app.listen(5000, console.log("Listening on 5000"));
